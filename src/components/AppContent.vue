21768bd7f931484268a91a56e94787157d366a2b
info-mic-info <jobsocial94@gmail.com>
2023-01-20 12:12:59 +0100

    add mixin

--- src/components/AppContent.vue
+++ src/components/AppContent.vue
@@ -99,6 +99,7 @@ export default {
 
 <style lang="scss" >
 @use "../styles/partials/variables" as *;
+@use "../styles/partials/mixin" as *;
 
 .flag {
   margin: auto 0;
@@ -123,19 +124,7 @@ strong {
 
   &:hover {
     .info {
-      font-size: 12px;
-      width: 100%;
-      height: 100%;
-      display: block;
-      position: absolute;
-      top: 50%;
-      left: 50%;
-      transform: translate(-50%, -50%);
-      color: white;
-      background-color: black;
-      opacity: 0.9;
-      border: 1px solid $redcolor;
-      border-radius: 10px;
+      @include infoHover;
     }
   }
 }
